e651a0cba0c62d2a47c4df8ca92ac1f4b20bc6ae
Nadhiradhit <nadhiradhitya@gmail.com>
2024-11-07 21:00:48 +0700

    refactor: switch from component blade to using a section

--- resources/views/landing/index.blade.php
+++ resources/views/landing/index.blade.php
@@ -2,18 +2,15 @@
 
 <x-layouts.app-layout>
     <section class="w-full">
-        <div class="flex flex-col gap-4 md:flex-row">
-            <div class="w-full">
-                <img src="../assets/images/campus-polimedia.jpg" alt="kampus-polimedia-img" class="w-full rounded-2xl h-[30rem] lg:h-[50rem]">
-            </div>
-            <div class="w-full p-8 bg-secondary rounded-2xl">
-                <div class="flex space-x-4">
-                    <p class="w-24 p-2 text-center text-slate-950 bg-gray-200 font-bold border-[3px] rounded-full border-slate-950">Humas</p>
+        <div class="flex flex-col lg:flex-row bg-secondary rounded-2xl">
+            <img src="../assets/images/campus-polimedia.jpg" alt="kampus-polimedia-img" class="image-index">
+            <div class="p-4 lg:p-8">
+                <div class="flex items-center pt-4 space-x-4">
+                    <p class="w-24 p-2 text-center text-slate-950 font-bold bg-gray-200 border-[3px] rounded-full border-slate-950">Humas</p>
                     <p class="w-32 p-2 text-center text-slate-950 font-bold bg-red-200 border-[3px] rounded-full border-slate-950">Polimedia</p>
-                    <p class="w-32 p-2 text-center text-slate-950 font-bold bg-slate-50 border-[3px] rounded-full border-slate-950">Beranda</p>
                 </div>
-                <div class="flex flex-col pt-12 h-[30rem]">
-                    <h2 class="font-bold lg:text-6xl text-slate-900">Humas Polimedia</h2>
+                <div class="flex flex-col pt-8 h-[30rem]">
+                    <h2 class="text-4xl font-bold lg:text-6xl text-slate-900">Humas Polimedia</h2>
                     <p class="pt-8 font-semibold text-justify lg:text-lg ">Lorem ipsum dolor sit amet consectetur adipisicing elit.
                         Quibusdam, optio facere sunt ex ratione voluptatum minima debitis iste totam
                         molestias veniam nam, voluptatibus illum, quas sint? Harum eius id
@@ -28,7 +25,14 @@
                 </div>
             </div>
         </div>
-        <h1>Hello</h1>
-        <p>This is landing page</p>
+        <div class="flex w-full gap-4 py-4">
+            <div class="w-full p-8 bg-accent rounded-2xl">
+                <h2 class="text-2xl font-bold w-96 p-2 text-center text-slate-950 bg-red-200 border-[3px] rounded-full border-slate-950 mb-4">Struktur Organisasi</h2>
+                <img src="../assets/images/struktur-organisasi.png" alt="struktur-organisasi-humas" class="rounded-2xl">
+            </div>
+            <div class="w-full p-8 bg-primary rounded-2xl">
+                <h2>Struktur Organisasi</h2>
+            </div>
+        </div>
     </section>
 </x-layouts.app-layout>

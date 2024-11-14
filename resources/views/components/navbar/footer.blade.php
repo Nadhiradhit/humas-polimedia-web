fc842180dec59b1d01a0c1fe378897b483ccb9bb
Nadhiradhit <nadhiradhitya@gmail.com>
2024-11-14 13:20:54 +0700

    feat: footer in user landing

---
+++ resources/views/components/navbar/footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="p-4 footer footer-center bg-secondary text-base-content border-[3px] border-slate-950 rounded-2xl mb-4">
+    <aside>
+        <p id="footer-text"></p>
+    </aside>
+</footer>
+
+<script>
+    document.getElementById('footer-text').innerText = `Copyright © ${new Date().getFullYear()} - All rights reserved by Humas Politeknik Negeri Media Kreatif`;
+</script>

469729d68148dcd187d94c0464039fde23fc9f10
Nadhiradhit <nadhiradhitya@gmail.com>
2024-12-09 20:44:13 +0700

    refactor: swith off a npm run build system to use vite

--- resources/views/components/layouts/details-layout-admin.blade.php
+++ resources/views/components/layouts/details-layout-admin.blade.php
@@ -5,8 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="icon" type="image/x-icon" href="{{ url('assets/images/polimedia-logo.png')}}">
-    <link rel="stylesheet" href="{{ asset('build/assets/app.css') }}">
-    <script src="{{ asset('build/assets/app.js') }}" defer></script>
+    {{-- <link rel="stylesheet" href="{{ asset('build/assets/app.css') }}">
+    <script src="{{ asset('build/assets/app.js') }}" defer></script> --}}
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     <title>@yield('title')</title>
 </head>
 <body class="p-4 mx-auto max-w-screen-2xl font-plusJakarta">

fac6094302283f432d5ad7ece6b3c2fc1ef4119d
Nadhiradhit <nadhiradhitya@gmail.com>
2024-12-09 20:43:44 +0700

    feat: form select component

--- resources/views/components/forms/form-select.blade.php
+++ resources/views/components/forms/form-select.blade.php
@@ -1,3 +1,24 @@
-<div>
-    <!-- It is not the man who has too little, but the man who craves more, that is poor. - Seneca -->
-</div>
+@props([
+    'name',
+    'id' => null,
+    'options' => [ ],
+    'selected' => null,
+    'placeholder',
+])
+<select
+        name="{{ $name }}"
+        id="{{ $id ?? $name }}"
+        {{ $attributes->merge(['class' => 'select select-primary w-full max-w-xs']) }}
+    >
+
+        @if($placeholder)
+            <option value="" disabled {{ $selected === null ? 'selected' : '' }}>
+                {{ $placeholder }}
+            </option>
+        @endif
+        @foreach($options as $value => $label)
+            <option value="{{ $value }}" {{ $value == $selected ? 'selected' : '' }}>
+                {{ $label }}
+            </option>
+        @endforeach
+</select>
